f94e78f990f287a37fde39ce3394a82a98477200
Brandon Andreis <bjandreis@gmail.com>
2020-08-04 09:36:40 -0400

    Updates

---
+++ precache-manifest.a80592104fb2e1da4748ddcfb202f462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91548db3c1a10f577d09be792300ee76",
+    "url": "/react-deploy-gh-page/index.html"
+  },
+  {
+    "revision": "17478972fb0b26ffa938",
+    "url": "/react-deploy-gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d48b8b48c803daff819",
+    "url": "/react-deploy-gh-page/static/js/2.3873ecb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-gh-page/static/js/2.3873ecb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17478972fb0b26ffa938",
+    "url": "/react-deploy-gh-page/static/js/main.8ed7c96e.chunk.js"
+  },
+  {
+    "revision": "98312882e7b933e22fb7",
+    "url": "/react-deploy-gh-page/static/js/runtime-main.5233c98b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
